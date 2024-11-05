fe1c8448beb1675e86977b4d7b7929e7cff73aab
Rio <126503340+its-rio@users.noreply.github.com>
2024-11-05 11:08:25 +0530

    rename the file
    according to its functionality

--- demoMavenproject/src/test/java/first1/DataDrivenFramework.java
+++ demoMavenproject/src/test/java/first1/DataDrivenFramework.java
@@ -13,7 +13,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class importFromfile {
+public class DataDrivenFramework {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
